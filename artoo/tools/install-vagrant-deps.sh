63a642f3eb6f6b1cde29a8b0e86c0eae0ca20362
Buzz <davidbuzz@gmail.com>
2017-07-30 10:34:43 +1000

    TURNOVER: Update GPG keys
    Need to explicitly get older gpg key/s from the keyserver or we get this error:
    GPG error: http://ppa.launchpad.net precise Release: The following signatures couldn't be verified because the public key is not available: NO_PUBKEY 6D1D8367A3421AFB

--- artoo/tools/install-vagrant-deps.sh
+++ artoo/tools/install-vagrant-deps.sh
@@ -1,3 +1,5 @@
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF0E1940624A220
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6D1D8367A3421AFB
 
 apt-add-repository 'deb http://ppa.launchpad.net/anatol/tup/ubuntu precise main' -y
 add-apt-repository 'ppa:terry.guo/gcc-arm-embedded' -y
