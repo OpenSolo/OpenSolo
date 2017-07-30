aaaa265984323cef328d51a976eac6edbc024a2a
Buzz <davidbuzz@gmail.com>
2017-07-30 17:03:54 +1000

    machine name should be - hyphens, not underscores , no idea why it was underscores, maybe historical.?   also change text so it say we default to master, which is what we are doing now.

--- solo-builder/builder.sh
+++ solo-builder/builder.sh
@@ -3,7 +3,7 @@
 cd /solo-build
 
 if [ -z "$1" ]; then
-    BRANCH=sololink_v2.1.0-2
+    BRANCH=master
 else
     BRANCH=$1
 fi
@@ -14,8 +14,8 @@ echo "Initializing repo with branch $BRANCH"
 repo init -u https://github.com/OpenSolo/3dr-arm-yocto-bsp.git 
 repo sync
 
-export MACHINE=imx6solo_3dr_1080p
+export MACHINE=imx6solo-3dr-1080p
 EULA=1 source ./setup-environment build
 
-MACHINE=imx6solo_3dr_1080p bitbake 3dr-solo
-MACHINE=imx6solo_3dr_artoo bitbake 3dr-controller
+MACHINE=imx6solo-3dr-1080p bitbake 3dr-solo
+MACHINE=imx6solo-3dr-artoo bitbake 3dr-controller
